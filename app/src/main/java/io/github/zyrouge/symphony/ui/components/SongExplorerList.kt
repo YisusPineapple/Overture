6a59667ee8c77ee4a1c6daced9440927ebe483de
Yisus Pineapple <jesuspina0930@gmail.com>
2026-06-28 08:25:22 -0400

    fix(ui): resolve missing PaddingValues import in SongExplorerList
    - Added missing `androidx.compose.foundation.layout.PaddingValues` import to fix compilation error after increasing the bottom padding for the mini-player.

--- app/src/main/java/io/github/zyrouge/symphony/ui/components/SongExplorerList.kt
+++ app/src/main/java/io/github/zyrouge/symphony/ui/components/SongExplorerList.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
